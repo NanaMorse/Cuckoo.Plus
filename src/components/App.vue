8095e2238340b631e08f211557053f34136d4d9f
Nana Morse <nana.morse@outlook.com>
2018-10-09 23:37:41 +0800

    Update: try to get access token, step 1

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,37 @@
 <template>
   <div>
-    Hello Vue
+    <!-- todo add vue i18n -->
+    <h1>Please input the mastodon server which you want to join in..</h1>
+
+    <el-input placeholder="https://pawoo.net" :value="mastodonServer"/>
+
+    <el-button class="submit-server-name-btn" @click="onSubmitServerName">Submit</el-button>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import { Component } from 'vue-property-decorator'
-  import {} from 'vuex-class'
+  import { Vue, Component } from 'vue-property-decorator'
+  import { postClientInfo } from '@/api'
 
-  @Component
+  // the first step, ask for mastodon OAuth Access token
+  // and store this token
+
+  @Component({})
   class App extends Vue {
 
+    mastodonServer: string = 'https://pawoo.net';
+
+    async onSubmitServerName () {
+      const result = await postClientInfo(this.mastodonServer)
+    }
   }
 
   export default App
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  .submit-server-name-btn {
+    // todo don't use px
+    margin-top: 20px;
+  }
 </style>
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import * as ElementUI from 'element-ui'
+import VueResource from 'vue-resource'
 import App from './components/App.vue'
 
 Vue.use(ElementUI);
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
