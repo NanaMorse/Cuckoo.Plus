d000ab094fc07a659266f3103551c7fafef2c63f
KTachibanaM <whj19931115@gmail.com>
2018-10-19 08:16:11 -0700

    fix tests

--- src/formatter.spec.ts
+++ src/formatter.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha'
 import { expect } from 'chai'
-import { StringReplacer, insertHtmlDelsToText } from './formatter'
+import { StringReplacer, insertDels } from './formatter'
 
 describe('StringReplacer', () => {
     it('replaces char at middle with a string', () => {
@@ -29,52 +29,52 @@ describe('StringReplacer', () => {
 
 describe('insertHtmlDelsToText', () => {
     it('ignores strings with no dashes', () => {
-        expect(insertHtmlDelsToText("aaaaaa")).to.equal("aaaaaa")
+        expect(insertDels("aaaaaa")).to.equal("aaaaaa")
     })
     it('ignores strings with a single dash', () => {
-        expect(insertHtmlDelsToText("-aaaaaa")).to.equal("-aaaaaa")
-        expect(insertHtmlDelsToText("aaaaaa-")).to.equal("aaaaaa-")
-        expect(insertHtmlDelsToText("aaa-aaa")).to.equal("aaa-aaa")
-        expect(insertHtmlDelsToText("aaa -aaa")).to.equal("aaa -aaa")
-        expect(insertHtmlDelsToText("aaa- aaa")).to.equal("aaa- aaa")
-        expect(insertHtmlDelsToText("aaa - aaa")).to.equal("aaa - aaa")
+        expect(insertDels("-aaaaaa")).to.equal("-aaaaaa")
+        expect(insertDels("aaaaaa-")).to.equal("aaaaaa-")
+        expect(insertDels("aaa-aaa")).to.equal("aaa-aaa")
+        expect(insertDels("aaa -aaa")).to.equal("aaa -aaa")
+        expect(insertDels("aaa- aaa")).to.equal("aaa- aaa")
+        expect(insertDels("aaa - aaa")).to.equal("aaa - aaa")
     })
     it('format strings with a pair of correct dashes', () => {
-        expect(insertHtmlDelsToText("-aaaaaa-")).to.equal("<del>aaaaaa</del>")
-        expect(insertHtmlDelsToText("-aaa- aaa")).to.equal("<del>aaa</del> aaa")
-        expect(insertHtmlDelsToText("aaa -aaa-")).to.equal("aaa <del>aaa</del>")
-        expect(insertHtmlDelsToText("aa -aa- aa")).to.equal("aa <del>aa</del> aa")
+        expect(insertDels("-aaaaaa-")).to.equal("<del>aaaaaa</del>")
+        expect(insertDels("-aaa- aaa")).to.equal("<del>aaa</del> aaa")
+        expect(insertDels("aaa -aaa-")).to.equal("aaa <del>aaa</del>")
+        expect(insertDels("aa -aa- aa")).to.equal("aa <del>aa</del> aa")
     })
     it('ignore strings with a pair of wrong dashes', () => {
-        expect(insertHtmlDelsToText("-aaa-aaa")).to.equal("-aaa-aaa")
-        expect(insertHtmlDelsToText("aaa-aaa-")).to.equal("aaa-aaa-")
-        expect(insertHtmlDelsToText("aa-aa-aa")).to.equal("aa-aa-aa")
+        expect(insertDels("-aaa-aaa")).to.equal("-aaa-aaa")
+        expect(insertDels("aaa-aaa-")).to.equal("aaa-aaa-")
+        expect(insertDels("aa-aa-aa")).to.equal("aa-aa-aa")
     })
     it('format string with a pair of correct dashes and a wrong dash', () => {
-        expect(insertHtmlDelsToText("-aaa-aaa-")).to.equal("<del>aaa-aaa</del>")
-        expect(insertHtmlDelsToText("-a-aa- aaa")).to.equal("<del>a-aa</del> aaa")
-        expect(insertHtmlDelsToText("-aaa- a-aa")).to.equal("<del>aaa</del> a-aa")
-        expect(insertHtmlDelsToText("aaa -a-aa-")).to.equal("aaa <del>a-aa</del>")
-        expect(insertHtmlDelsToText("a-aa -aaa-")).to.equal("a-aa <del>aaa</del>")
-        expect(insertHtmlDelsToText("aa -a-a- aa")).to.equal("aa <del>a-a</del> aa")
-        expect(insertHtmlDelsToText("a-a -aa- aa")).to.equal("a-a <del>aa</del> aa")
+        expect(insertDels("-aaa-aaa-")).to.equal("<del>aaa-aaa</del>")
+        expect(insertDels("-a-aa- aaa")).to.equal("<del>a-aa</del> aaa")
+        expect(insertDels("-aaa- a-aa")).to.equal("<del>aaa</del> a-aa")
+        expect(insertDels("aaa -a-aa-")).to.equal("aaa <del>a-aa</del>")
+        expect(insertDels("a-aa -aaa-")).to.equal("a-aa <del>aaa</del>")
+        expect(insertDels("aa -a-a- aa")).to.equal("aa <del>a-a</del> aa")
+        expect(insertDels("a-a -aa- aa")).to.equal("a-a <del>aa</del> aa")
     })
     it('format string with a pair of correct dashes and a correct dash', () => {
-        expect(insertHtmlDelsToText("-a -aa- aaa")).to.equal("<del>a -aa</del> aaa")
-        expect(insertHtmlDelsToText("-aaa- a -aa")).to.equal("<del>aaa</del> a -aa")
-        expect(insertHtmlDelsToText("aaa -a -aa-")).to.equal("aaa <del>a -aa</del>")
-        expect(insertHtmlDelsToText("a- aa -aaa-")).to.equal("a- aa <del>aaa</del>")
-        expect(insertHtmlDelsToText("aa -a- a- aa")).to.equal("aa <del>a- a</del> aa")
-        expect(insertHtmlDelsToText("aa -a -a- aa")).to.equal("aa <del>a -a</del> aa")
-        expect(insertHtmlDelsToText("a- a -aa- aa")).to.equal("a- a <del>aa</del> aa")
+        expect(insertDels("-a -aa- aaa")).to.equal("<del>a -aa</del> aaa")
+        expect(insertDels("-aaa- a -aa")).to.equal("<del>aaa</del> a -aa")
+        expect(insertDels("aaa -a -aa-")).to.equal("aaa <del>a -aa</del>")
+        expect(insertDels("a- aa -aaa-")).to.equal("a- aa <del>aaa</del>")
+        expect(insertDels("aa -a- a- aa")).to.equal("aa <del>a- a</del> aa")
+        expect(insertDels("aa -a -a- aa")).to.equal("aa <del>a -a</del> aa")
+        expect(insertDels("a- a -aa- aa")).to.equal("a- a <del>aa</del> aa")
     })
     it('format string with two pairs of correct dashes', () => {
-        expect(insertHtmlDelsToText("a -a- aa -a- a")).to.equal("a <del>a</del> aa <del>a</del> a")
-        expect(insertHtmlDelsToText("-aa- aa -a- a")).to.equal("<del>aa</del> aa <del>a</del> a")
-        expect(insertHtmlDelsToText("a -a- aa -aa-")).to.equal("a <del>a</del> aa <del>aa</del>")
-        expect(insertHtmlDelsToText("-aa- aa -aa-")).to.equal("<del>aa</del> aa <del>aa</del>")
+        expect(insertDels("a -a- aa -a- a")).to.equal("a <del>a</del> aa <del>a</del> a")
+        expect(insertDels("-aa- aa -a- a")).to.equal("<del>aa</del> aa <del>a</del> a")
+        expect(insertDels("a -a- aa -aa-")).to.equal("a <del>a</del> aa <del>aa</del>")
+        expect(insertDels("-aa- aa -aa-")).to.equal("<del>aa</del> aa <del>aa</del>")
     })
     it('format string with two enclosing pairs of correct dashes', () => {
-        expect(insertHtmlDelsToText("a -a -aa- a- a")).to.equal("a <del>a -aa- a</del> a")
+        expect(insertDels("a -a -aa- a- a")).to.equal("a <del>a -aa- a</del> a")
     })
 })
