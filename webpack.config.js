31e2d0dd715d896d27919554109e659445c7c665
Morse_Guo <nana.morse@outlook.com>
2018-10-25 14:43:51 +0800

    Update: use pre commit hook to build bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,7 @@ const plugins = [
 
 if (isEnvProduction) {
   // 删除source-map数据 / remove source map data
-  fs.unlink(path.join(__dirname, './public/dist/bundle.js.map'), (err) => {
-    if (err) console.error(err)
-  })
+  fs.unlink(path.join(__dirname, './public/dist/bundle.js.map'), (err) => {})
 } else {
   plugins.push(new BundleAnalyzerPlugin())
 }
