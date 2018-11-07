3069400a80b2291f89d48b43202106d69017b52c
Morse_Guo <nana.morse@outlook.com>
2018-11-07 17:14:40 +0800

    temp

--- src/components/PostStatusDialog.vue
+++ src/components/PostStatusDialog.vue
@@ -53,10 +53,19 @@
                 :placeholder="$t($i18nTags.statusCard.post_new_status_placeholder)"/>
 
       <div class="bottom-area">
-        <div class="media-preview-area"></div>
+        <div v-if="uploadProcessInfoList.length" class="media-preview-area" :class="{ 'single-upload-preview-area': uploadProcessInfoList.length === 1 }">
+          <div class="media-item" :key="index"
+               v-for="(processInfo, index) in uploadProcessInfoList">
+            <div v-if="!processInfo.uploadSuccess" class="media-placeholder" v-loading="true"/>
+            <img v-if="processInfo.uploadSuccess" :src="processInfo.uploadResult.url"/>
+            <div class="remove-icon-wrapper">
+              <svg height="24px" width="24px" viewBox="0 0 48 48"><circle fill="#fefefe" cx="24" cy="24" r="24"></circle><path fill="#000" d="M24,4C12.9,4,4,12.9,4,24s8.9,20,20,20s20-9,20-20S35,4,24,4z M34,31.2L31.2,34L24,26.8L16.8,34L14,31.2l7.2-7.2L14,16.8l2.8-2.8l7.2,7.2l7.2-7.2l2.8,2.8L26.8,24L34,31.2z"></path></svg>
+            </div>
+          </div>
+        </div>
 
         <div class="attachment-select-btn-group">
-          <mu-button icon @click="onSelectMediaFiles">
+          <mu-button icon @click="onSelectMediaFiles" :disabled="uploadProcessInfoList.length === 4">
             <mu-icon class="secondary-read-text-color" value="camera_alt" />
             <input ref="fileInput" type="file" @change="onUploadMediaFiles"
                    accept=".jpg,.jpeg,.png,.gif,.webm,.mp4,.m4v,.mov,image/jpeg,image/png,image/gif,video/webm,video/mp4,video/quicktime"
@@ -89,9 +98,12 @@
   import { getVisibilityDescInfo } from '@/util'
   import VisibilitySelectPopOver from '@/components/VisibilitySelectPopOver'
   import * as Api from '@/api'
+  import { mastodonentities } from "../interface";
   const autosize = require('autosize')
 
   let isFirstTimeOpenDialog = true
+
+  const preViewAreaHeight = 212
 
   @Component({
     components: {
@@ -114,7 +126,11 @@
 
     shouldOpenVisibilitySelectPopOver = false
 
-    fileListToUpload: Array<File> = []
+    uploadProcessInfoList: Array<{
+      file: File,
+      uploadSuccess: boolean,
+      uploadResult: mastodonentities.Attachment
+    }> = []
 
     textContentValue: string = ''
 
@@ -138,6 +154,12 @@
 
     get shouldEnableSubmitButton () {
       return this.textContentValue
+    }
+
+    getMediaItemStyle (resultInfo: mastodonentities.Attachment) {
+      if (!resultInfo) return { width: 'auto' }
+
+      return
     }
 
     @Watch('isDialogOpening')
@@ -170,7 +192,7 @@
     }
 
     async onTryCloseDialog () {
-      if (this.textContentValue) {
+      if (this.textContentValue || this.uploadProcessInfoList.length) {
         // @ts-ignore todo i18n
         const doCloseDialog = (await this.$confirm('要舍弃这条信息吗？', '', {})).result
         if (doCloseDialog) {
@@ -203,12 +225,32 @@
     }
 
     onUploadMediaFiles () {
-      console.dir(this.$refs.fileInput.files)
-      // todo test
-      const formData = new FormData()
-      formData.append('file', this.$refs.fileInput.files[0])
+      const maxUploadLength = 4
 
-      Api.media.postMediaFile(formData)
+      Array.from(this.$refs.fileInput.files)
+        .splice(0, maxUploadLength - this.uploadProcessInfoList.length)
+        .forEach(async (file) => {
+          this.uploadProcessInfoList.push({
+            file, uploadSuccess: false, uploadResult: null
+          })
+
+          const indexInProcessList = this.uploadProcessInfoList.length - 1
+
+          const formData = new FormData()
+          formData.append('file', file)
+
+          try {
+            const result = await Api.media.postMediaFile(formData)
+
+            this.uploadProcessInfoList[indexInProcessList].uploadSuccess = true
+            this.uploadProcessInfoList[indexInProcessList].uploadResult = result.data
+
+          } catch (e) {
+
+          }
+
+        })
+
     }
 
     setVisibilitySelectPopOverDisplay (open: boolean) {
@@ -314,6 +356,44 @@
 
       .media-preview-area {
         height: 212px;
+        display: flex;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        padding: 0 16px;
+
+        &.single-upload-preview-area {
+          .media-item {
+            margin: 0;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+          }
+        }
+
+        .media-item {
+          margin-right: 8px;
+          position: relative;
+          object-fit: contain;
+
+          .media-placeholder {
+            width: 212px;
+            position: relative;
+            height: 100%;
+          }
+
+          img {
+            width: auto;
+            height: 100%;
+            display: block;
+          }
+
+          .remove-icon-wrapper {
+            cursor: pointer;
+            position: absolute;
+            right: 12px;
+            top: 12px;
+          }
+        }
       }
     }
 
