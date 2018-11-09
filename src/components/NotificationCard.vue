a0476c19aff1ecb3041a2a5799f5bb05c39efe24
KTachibanaM <whj19931115@gmail.com>
2018-11-09 09:29:00 -0800

    show notification count icon

--- src/components/NotificationCard.vue
+++ src/components/NotificationCard.vue
@@ -1,27 +1,31 @@
 <template>
-  <mu-container>
-    <mu-card style="width: 100%; max-width: 375px; margin: 0 auto;">
-      <mu-card-header :title="title" :sub-title="subTitle">
-        <mu-avatar slot="avatar">
-          <img :src="avatarLink">
-        </mu-avatar>
-      </mu-card-header>
-    </mu-card>
-  </mu-container>
+  <mu-card style="width: 100%; max-width: 530px; margin: 0 auto; cursor: pointer;" @click="onClickCard">
+    <mu-card-header :title="title" :sub-title="subTitle">
+      <mu-avatar slot="avatar">
+        <img :src="avatarLink">
+      </mu-avatar>
+    </mu-card-header>
+  </mu-card>
 </template>
 
 <script lang="ts">
   import { Vue, Component, Prop } from 'vue-property-decorator'
-  import { Getter } from 'vuex-class'
+  import { Getter, Mutation } from 'vuex-class'
   import { mastodonentities } from '@/interface'
   import { extractText } from "../util";
 
   @Component({})
   class NotificationCard extends Vue {
 
+    $router
+
+    $routersInfo
+
     @Prop() notification: mastodonentities.Notification
 
     @Getter('getAccountDisplayName') getAccountDisplayName
+
+    @Mutation('updateNotificationsPanelStatus') updateNotificationsPanelStatus
 
     get title() {
       return this.getAccountDisplayName(this.notification.account)
@@ -33,6 +37,16 @@
 
     get avatarLink() {
       return this.notification.account.avatar_static
+    }
+
+    onClickCard() {
+      this.updateNotificationsPanelStatus(false)
+      this.$router.push({
+        name: this.$routersInfo.statuses.name,
+        params: {
+          statusId: this.notification.status.id
+        }
+      })
     }
   }
 
