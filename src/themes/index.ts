269713cb34397167d42d2646ba42d3f3e38b28c2
Morse_Guo <nana.morse@outlook.com>
2019-03-17 17:24:54 +0800

    Resolved #148

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -12,6 +12,11 @@ class ThemeManager {
     [ThemeNames.DARK]: DarkTheme
   }
 
+  private themeToFavIconSourceMap = {
+    [ThemeNames.GOOGLE_PLUS]: 'google_plus',
+    [ThemeNames.DARK]: 'dark'
+  }
+
   private getThemeStyleElem (): HTMLStyleElement {
     const themeElemId = 'cuckoo-plus-theme'
     let styleElem = document.getElementById(themeElemId)
@@ -25,8 +30,21 @@ class ThemeManager {
     return styleElem as HTMLStyleElement
   }
 
+  private setFavIconByThemeName (themeName: string) {
+    Array.from(document.head.querySelectorAll('link')).forEach(el => {
+      if (el.getAttribute('rel') === 'icon') {
+        const size = el.getAttribute('sizes')
+        if (size) {
+          el.setAttribute('href', `favicon/${this.themeToFavIconSourceMap[themeName]}/favicon-${size}.png`)
+        }
+      }
+    })
+  }
+
   public setTheme (themeName: string) {
     this.getThemeStyleElem().innerHTML = this.themeMap[themeName] || ''
+
+    this.setFavIconByThemeName(themeName)
   }
 }
 
