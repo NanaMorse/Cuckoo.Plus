b3f677b096030e3bdbfa57ed184005114fa5999e
Nana Morse <nana.morse@outlook.com>
2019-03-25 17:54:01 +0800

    tesst

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 npm run build
 git add .
 git commit -m "build"
-git push origin deploy --quiet
-ssh -i "light-sail-cuckoo-plus.pem" ubuntu@52.76.67.104 "cd projects/Cuckoo.Plus; git fetch; git checkout -b deploy --track origin/deploy; git pull"
+scp -i "light-sail-cuckoo-plus.pem" -r public ubuntu@52.76.67.104:/projects/Cuckoo.Plus/
